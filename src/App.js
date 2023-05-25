4d6b69cb844ff89e8ffc8dc1decf0f0c5070b3c8
emirhankorhan <korhanemirhann@gmail.com>
2023-05-25 23:56:56 +0300

    commit try

--- src/App.js
+++ src/App.js
@@ -1,88 +1,19 @@
-import './App.css';
-import { useState, React } from "react";
-import Content from './components/content/Content';
-// import Features from './components/features/Features';
-// import Featurestwo from './components/featurestwo/Featurestwo';
+import { Route, Routes } from "react-router-dom";
 
+import './App.css';
+import Header from "./components/Header/Header";
+import Focus from "./components/focus/Focus";
 
 function App() {
 
-  const [onButton, setOnButton] = useState(false);
-
-  const menuIsOpen = () => {
-    setOnButton(true);
-  }
-
-  const menuIsClose = () => {
-    setOnButton(false);
-  }
-
-  if (onButton === false) {
-    return (
-      <div className='app'>
-        <div className='header-navbar'>
-          <div className='header-nav-logo'>
-
-            <p className='first-logo'>brood</p>
-          </div>
-          <button onClick={menuIsOpen} className='menu-button'><i class="fa-solid fa-layer-group"></i></button>
-          <div className='header-nav-link'>
-            <a href='/'>Home</a>
-            <a href='/features'>Features</a>
-            <a href='/pricing'>Pricing</a>
-            <a href='/contact'>Contact</a>
-          </div>
-          <div className='header-nav-log'>
-            <button className='sign-button'>Sign in</button>
-            <button className='login-button'>Login</button>
-            
-          </div>
-        </div>
-        <Content />
-      </div>
-    )
-
-  } else if (onButton === true) {
-    return (
-      <div className='app'>
-
-        <div className='menu-open'>
-
-          <div className='close-button'><i onClick={menuIsClose} class="fa-solid fa-xmark"></i></div>
-          <div className='header-nav-close-link'>
-            <a href='/'>Home</a>
-            <a href='/features'>Features</a>
-            <a href='/pricing'>Pricing</a>
-            <a href='/contact'>Contact</a>
-          </div>
-
-          <div className='header-nav-close-log'>
-            <button className='sign-button'>Sign in</button>
-            <button className='login-button'>Login</button>
-          </div>
-
-        </div>
-        <div className='header-navbar'>
-          <div className='header-nav-logo'>
-
-            <p className='first-logo'>brood</p>
-          </div>
-          <button onClick={menuIsOpen} className='menu-button'><i class="fa-solid fa-layer-group"></i></button>
-          <div className='header-nav-link'>
-            <a href='/'>Home</a>
-            <a href='/features'>Features</a>
-            <a href='/pricing'>Pricing</a>
-            <a href='/contact'>Contact</a>
-          </div>
-          <div className='header-nav-log'>
-            <button className='sign-button'>Sign in</button>
-            <button className='login-button'>Login</button>
-          </div>
-        </div>
-        <Content />
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Header />} />
+        <Route path='/focus' element={<Focus />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,91 @@
-// import { useState, React } from "react";
-// import "./header.css"
+import './header.css';
+import { useState, React } from "react";
+import Content from "../content/Content"
+// import Features from './components/features/Features';
+// import Featurestwo from './components/featurestwo/Featurestwo';
 
-// function Header() {
 
-  
+function Header() {
 
+  const [onButton, setOnButton] = useState(false);
+ 
 
-  
-  
-  
-// }
+  const menuIsOpen = () => {
+    setOnButton(true);
+    
+  }
 
-// export default Header
+  const menuIsClose = () => {
+    setOnButton(false);
+    
+  }
+
+  if (onButton === false) {
+    return (
+      <div className='app'>
+        <div className='header-navbar'>
+          <div className='header-nav-logo'>
+
+            <p className='first-logo'>brood</p>
+          </div>
+          <button onClick={menuIsOpen} className='menu-button'><i class="fa-solid fa-layer-group"></i></button>
+          <div className='header-nav-link'>
+            <a href='/'>Home</a>
+            <a href='/features'>Features</a>
+            <a href='/pricing'>Pricing</a>
+            <a href='/contact'>Contact</a>
+          </div>
+          <div className='header-nav-log'>
+            <button className='sign-button'>Sign in</button>
+            <button className='login-button'>Login</button>
+            
+          </div>
+        </div>
+        <Content />
+      </div>
+    )
+
+  } else if (onButton === true) {
+    return (
+      <div className='app'>
+
+        <div className="menu-open">
+
+          <div className="close-button"><i onClick={menuIsClose} class="fa-solid fa-xmark"></i></div>
+          <div className='header-nav-close-link'>
+            <a href='/'>Home</a>
+            <a href='/features'>Features</a>
+            <a href='/pricing'>Pricing</a>
+            <a href='/contact'>Contact</a>
+          </div>
+
+          <div className='header-nav-close-log'>
+            <button className='sign-button'>Sign in</button>
+            <button className='login-button'>Login</button>
+          </div>
+
+        </div>
+        <div className='header-navbar'>
+          <div className='header-nav-logo'>
+
+            <p className='first-logo'>brood</p>
+          </div>
+          <button onClick={menuIsOpen} className='menu-button'><i class="fa-solid fa-layer-group"></i></button>
+          <div className='header-nav-link'>
+            <a href='/'>Home</a>
+            <a href='/features'>Features</a>
+            <a href='/pricing'>Pricing</a>
+            <a href='/contact'>Contact</a>
+          </div>
+          <div className='header-nav-log'>
+            <button className='sign-button'>Sign in</button>
+            <button className='login-button'>Login</button>
+          </div>
+        </div>
+        <Content />
+      </div>
+    )
+  }
+}
+
+export default Header;
