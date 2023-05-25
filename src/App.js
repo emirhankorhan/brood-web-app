e173b0a05094bc070232df69c64c6aae8350ede5
emirhankorhan <korhanemirhann@gmail.com>
2023-05-25 19:10:57 +0300

    menu edit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
 import './App.css';
-import Header from './components/Header/Header';
+import { useState, React } from "react";
 import Content from './components/content/Content';
 // import Features from './components/features/Features';
 // import Featurestwo from './components/featurestwo/Featurestwo';
 
 function App() {
 
-  return (
-    <div>
-      <div className="app">
-        <Header />
-        <Content />
+  const [onButton, setOnButton] = useState(false);
+
+  const menuIsOpen = () => {
+    setOnButton(true);
+  }
+
+  const menuIsClose = () => {
+    setOnButton(false);
+  }
+
+  if(onButton === false){
+    return (
+      <div className='app'>
+      <div className='header-navbar'>
+          <div className='header-nav-logo'>
+            
+              <p className='first-logo'>brood</p>
+          </div>
+          <button onClick={menuIsOpen} className='menu-button'><i class="fa-solid fa-grip-lines"></i></button>
+          <div className='header-nav-link'>
+              <a href='/'>Home</a>
+              <a href='/features'>Features</a>
+              <a href='/pricing'>Pricing</a>
+              <a href='/contact'>Contact</a>
+          </div>
+          <div className='header-nav-log'>
+              <button className='sign-button'>Sign in</button>
+              <button className='login-button'>Login</button>
+          </div>
       </div>
-      {/* <Features />
-      <Featurestwo/> */}
+      <Content/>
+      </div>
+    )
+
+  }else if(onButton === true){
+    return(
+      <div className='app'>
+    <div className='menu-open'>
+      
+      <div className='close-button'><i onClick={menuIsClose} class="fa-solid fa-xmark"></i></div>
+      <div className='header-nav-close-link'>
+              <a href='/'>Home</a>
+              <a href='/features'>Features</a>
+              <a href='/pricing'>Pricing</a>
+              <a href='/contact'>Contact</a>
+          </div>
+
+          <div className='header-nav-close-log'>
+              <button className='sign-button'>Sign in</button>
+              <button className='login-button'>Login</button>
+          </div>
+
     </div>
-  );
+    <Content/>
+    </div>
+    )
+  }
 }
 
 export default App;
