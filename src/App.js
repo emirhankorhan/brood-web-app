822e77ab23d6062ac2d908ae3963094240d6460e
emirhankorhan <korhanemirhann@gmail.com>
2023-05-25 23:28:16 +0300

    redirect edit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Content from './components/content/Content';
 // import Features from './components/features/Features';
 // import Featurestwo from './components/featurestwo/Featurestwo';
 
+
 function App() {
 
   const [onButton, setOnButton] = useState(false);
@@ -16,69 +17,70 @@ function App() {
     setOnButton(false);
   }
 
-  if(onButton === false){
+  if (onButton === false) {
     return (
       <div className='app'>
-      <div className='header-navbar'>
+        <div className='header-navbar'>
           <div className='header-nav-logo'>
-            
-              <p className='first-logo'>brood</p>
+
+            <p className='first-logo'>brood</p>
           </div>
           <button onClick={menuIsOpen} className='menu-button'><i class="fa-solid fa-layer-group"></i></button>
           <div className='header-nav-link'>
-              <a href='/'>Home</a>
-              <a href='/features'>Features</a>
-              <a href='/pricing'>Pricing</a>
-              <a href='/contact'>Contact</a>
+            <a href='/'>Home</a>
+            <a href='/features'>Features</a>
+            <a href='/pricing'>Pricing</a>
+            <a href='/contact'>Contact</a>
           </div>
           <div className='header-nav-log'>
-              <button className='sign-button'>Sign in</button>
-              <button className='login-button'>Login</button>
+            <button className='sign-button'>Sign in</button>
+            <button className='login-button'>Login</button>
+            
           </div>
-      </div>
-      <Content/>
+        </div>
+        <Content />
       </div>
     )
 
-  }else if(onButton === true){
-    return(
+  } else if (onButton === true) {
+    return (
       <div className='app'>
-        
-    <div className='menu-open'>
-      
-      <div className='close-button'><i onClick={menuIsClose} class="fa-solid fa-xmark"></i></div>
-      <div className='header-nav-close-link'>
-              <a href='/'>Home</a>
-              <a href='/features'>Features</a>
-              <a href='/pricing'>Pricing</a>
-              <a href='/contact'>Contact</a>
+
+        <div className='menu-open'>
+
+          <div className='close-button'><i onClick={menuIsClose} class="fa-solid fa-xmark"></i></div>
+          <div className='header-nav-close-link'>
+            <a href='/'>Home</a>
+            <a href='/features'>Features</a>
+            <a href='/pricing'>Pricing</a>
+            <a href='/contact'>Contact</a>
           </div>
 
           <div className='header-nav-close-log'>
-              <button className='sign-button'>Sign in</button>
-              <button className='login-button'>Login</button>
+            <button className='sign-button'>Sign in</button>
+            <button className='login-button'>Login</button>
           </div>
 
-    </div>
-    <div className='header-navbar'>
+        </div>
+        <div className='header-navbar'>
           <div className='header-nav-logo'>
-            
-              <p className='first-logo'>brood</p>
+
+            <p className='first-logo'>brood</p>
           </div>
           <button onClick={menuIsOpen} className='menu-button'><i class="fa-solid fa-layer-group"></i></button>
           <div className='header-nav-link'>
-              <a href='/'>Home</a>
-              <a href='/features'>Features</a>
-              <a href='/pricing'>Pricing</a>
-              <a href='/contact'>Contact</a>
+            <a href='/'>Home</a>
+            <a href='/features'>Features</a>
+            <a href='/pricing'>Pricing</a>
+            <a href='/contact'>Contact</a>
           </div>
           <div className='header-nav-log'>
-              <button className='sign-button'>Sign in</button>
-              <button className='login-button'>Login</button>
+            <button className='sign-button'>Sign in</button>
+            <button className='login-button'>Login</button>
           </div>
+        </div>
+        <Content />
       </div>
-    <Content/>
-    </div>
     )
   }
 }
---
+++ src/components/focus/Focus.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./focus.css"
+
+function Focus() {
+  return (
+    <div className='focus-div'>Focus</div>
+  )
+}
+
+export default Focus
